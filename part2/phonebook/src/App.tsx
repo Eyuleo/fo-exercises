a7966ead1a92bd23f5b7efdaa49339a28605320a
Eyuleo <eyuleo10@gmail.com>
2026-06-06 15:19:50 +0300

    done exercise 2.11

--- part2/phonebook/src/App.tsx
+++ part2/phonebook/src/App.tsx
@@ -1,18 +1,22 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
 import Person from "./components/Person"
+import axios from "axios"
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState("")
   const [phoneNumber, setPhoneNumber] = useState("")
   const [filter, setFilter] = useState("")
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data)
+      setLoading(false)
+    })
+  }, [])
 
   const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNewName(event.target.value)
@@ -62,6 +66,7 @@ const App = () => {
         handlePhoneChange={handlePhoneChange}
       />
       <h2>Numbers</h2>
+      {loading ? <p>Loading...</p> : null}
       <Person filteredPerson={filteredPerson} />
     </div>
   )
